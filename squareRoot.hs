e6fb61725c84ec5d9a1b795b0e042406eb3ea48b
swrangsar basumatary <swrangsar@example.com>
2013-11-01 08:47:15 +0530

    commit at 20131101084715

---
+++ squareRoot.hs
@@ -0,0 +1,20 @@
+-- using Newton's method and using the nearest power of 2 --
+
+numbits :: (Integral a) => a -> a
+numbits n
+    | n < 2     = 1
+    | otherwise = 1 + (numbits (n `div` 2))
+
+
+newtonsMethod :: (RealFrac a) => a -> a -> a
+newtonsMethod x n
+    | y < x         = newtonsMethod y n
+    | otherwise     = x
+    where y         = (x + (n/x))/2
+    
+    
+squareRoot :: (RealFrac a) => a -> a
+squareRoot n = newtonsMethod y n
+                where x = truncate n
+                      y = fromIntegral $ (2^) $ ceiling $ (toRational $ numbits(x)) / 2
+                
