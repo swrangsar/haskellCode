1dd4c2bfcb8675ff7ec0b0bd3f12c215e92d2cc6
swrangsar basumatary <swrangsar@example.com>
2013-11-12 18:25:45 +0530

    commit at 20131112182544

--- codedTriangleNumbers.hs
+++ codedTriangleNumbers.hs
@@ -1,16 +1,8 @@
 import Data.Char
 
 
-
-commaSepValues :: String -> [String]
-commaSepValues s = case dropWhile (==',') s of
-                        "" -> []
-                        s' -> w : commaSepValues s''
-                            where (w, s'') = break (==',') s'
-
-
-removeQuotes :: String -> String
-removeQuotes s = filter (/='"') s
+codedWords :: String -> [String]
+codedWords s =  words $ map (\y -> if y == ',' then ' ' else y) $ filter (/='"') s
 
 char2num :: Char -> Int
 char2num c = ord c - ord 'A' + 1
@@ -23,10 +15,14 @@ word2num = sum.(map char2num)
 isTriangle :: (Integral a) => a -> Bool
 isTriangle n
     | n <= 0 = False
-    | n == 1 = True
-    | t == (2*x) = True
+    | t == x = True
     | otherwise = False
     where t = root * (root + 1)
           root = (truncate.sqrt.(*2).fromIntegral) n
-          x = (truncate.fromIntegral) n
+          x = (truncate.(*2).fromIntegral) n
     
+codedNumbers :: String -> [Int]
+codedNumbers s =  map word2num $ codedWords s
+
+requiredCount :: String -> Int
+requiredCount s = length $ filter id $ map isTriangle $ codedNumbers s
