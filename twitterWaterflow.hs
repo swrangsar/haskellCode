b1e780867378afdb11124e29113b1b02381d36a2
swrangsar basumatary <swrangsar@example.com>
2013-11-14 13:08:48 +0530

    commit at 20131114130848

--- twitterWaterflow.hs
+++ twitterWaterflow.hs
@@ -1,2 +1,9 @@
 {-- ideas from http://philipnilsson.github.io/Badness10k/articles/waterflow/ [accessed oct,2013] --}
 
+waterFlow :: (Integral a) => [a] -> a
+waterFlow [] = 0
+waterFlow hs = sum waterHeights
+               where leftMaxs       = scanl1 max hs
+                     rightMaxs      = scanr1 max hs
+                     waterLevels    = zipWith min leftMaxs rightMaxs
+                     waterHeights   = zipWith (-) waterLevels hs
