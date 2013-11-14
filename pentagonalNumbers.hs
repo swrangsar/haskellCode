8b91b9c26ea94273e465c1c925a802e1e4563161
swrangsar basumatary <swrangsar@example.com>
2013-11-14 20:54:07 +0530

    commit at 20131114205407

--- pentagonalNumbers.hs
+++ pentagonalNumbers.hs
@@ -15,3 +15,22 @@ pentagonal n    = div nume 2
 
 candidates = [[a,b,d] | y <- [1..], x <- [1..(y-1)], x > 0, let a = pentagonal x, let b = pentagonal y, isPentagonal (a+b), let d = b - a, isPentagonal d]
 requiredCandidate = (!! 2) $ candidates !! 0
+
+isDiffPenta :: (Integral a) => a -> Bool
+isDiffPenta diff = smallGeneratorExist diff 1
+
+
+{-- equation 3ns = d - pentagonal(s) 
+    n is the pentagonal generator of the smaller number in the pair --}
+smallGeneratorExist :: (Integral a) => a -> a -> Bool
+smallGeneratorExist diff s
+    | nume < (3*s)              = False
+    | (mod nume (3*s)) == 0     = True
+    | otherwise                 = smallGeneratorExist diff (s+1)    
+    where nume  = diff - (pentagonal s)
+    
+
+minimumPentaDiff :: (Integral a) => [a]
+minimumPentaDiff = filter isDiffPenta $map pentagonal [1..]
+
+revisedCandidates =[[a,b,d] | y <- [1..], x <- [1..(y-1)], x > 0, let a = pentagonal x, let b = pentagonal y, let d = b - a, isPentagonal d, isDiffPenta d, isPentagonal (a+b)]
