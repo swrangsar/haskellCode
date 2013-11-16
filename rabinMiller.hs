0985e5f51e7b80a0b630c74091e2eb9699e3f737
swrangsar basumatary <swrangsar@example.com>
2013-11-16 21:26:23 +0530

    commit at 20131116212623

--- rabinMiller.hs
+++ rabinMiller.hs
@@ -5,4 +5,17 @@ reduceEven d s
     where d' = div d 2
     
 
-
+modularExpoR :: (Integral a) => a -> a -> a -> a -> a
+modularExpoR _ 0 _ r = r
+modularExpoR b e m r
+    | even e       = modularExpoR b' e' m r
+    | (not.even) e = modularExpoR b (e-1) m r'
+    where b'    = mod (b*b) m
+          e'    = div e 2
+          r'    = mod (r*b) m
+          
+          
+modularExpo :: (Integral a) => a -> a -> a -> a
+modularExpo _ 0 _ = 1
+modularExpo b 1 m = mod b m
+modularExpo b e m = modularExpoR b e m 1
