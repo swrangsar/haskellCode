1ea78a7081579d9ee793d39651a676f9bc538241
swrangsar basumatary <swrangsar@example.com>
2013-11-16 21:49:53 +0530

    commit at 20131116214953

--- rabinMiller.hs
+++ rabinMiller.hs
@@ -19,3 +19,18 @@ modularExpo :: (Integral a) => a -> a -> a -> a
 modularExpo _ 0 _ = 1
 modularExpo b 1 m = mod b m
 modularExpo b e m = modularExpoR b e m 1
+
+
+isPseudoPrimeForBase :: (Integral a) => a -> a -> Bool
+isPseudoPrimeForBase n a = pseudoPrimeTest t s n
+                           where (d,s) = reduceEven (n-1) 0
+                                 t = modularExpo a d n
+
+
+pseudoPrimeTest :: (Integral a) => a -> a -> a -> Bool
+pseudoPrimeTest t s n
+    | (t==1) || (t==(n-1))  = True
+    | s == 0                = False
+    | otherwise             = pseudoPrimeTest t' s' n
+    where t' = mod (t*t) n
+          s' = s-1
