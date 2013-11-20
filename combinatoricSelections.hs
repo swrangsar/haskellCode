25165b1a6fea629257e370bd86afb0d7b236a9d8
swrangsar basumatary <swrangsar@example.com>
2013-11-20 11:26:21 +0530

    commit at 20131120112621

--- combinatoricSelections.hs
+++ combinatoricSelections.hs
@@ -2,26 +2,17 @@ isCombMil :: (Integral a) => a -> a -> Bool
 isCombMil _ 0 = False
 isCombMil _ 1 = False
 isCombMil n r
-    | n == r            = False
-    | newNums == []     = False
-    | otherwise         = isProdMil newNums 1
-    where   nums    = [(max r (n-r))..n]
-            dens    = [2..(min r (n-r))]
-            reduceFactors list@(x:xs) y
-                | list == []        = []
-                | (mod x y) == 0    = (div x y):xs
-                | otherwise         = x:(reduceFactors xs y)
-
-            reducedNums ns ds
-                | ds == []      = ns
-                | otherwise     = reducedNums (reduceFactors ns l) ls
-                where   (l:ls)  = ds
-            newNums             = reducedNums nums dens
-            isProdMil (x:xs) r
-                | r > 1000000   = True
-                | xs == []      = False
-                | otherwise     = isProdMil xs (x*r)
-        
+    | (n-r) < 2         = False
+    | comb > 1e6        = True
+    | otherwise         = False
+    where   r'      = min r (n-r)
+            nums    = [(n-r'+1)..n]
+            dens    = [1..r']
+            comb    = product nums / product dens       
 
 
-               
+numOfCombMils :: (Integral a) => a -> a
+numOfCombMils n = fromIntegral.length $ filter (isCombMil n) [0..n]
+
+
+reqdCount = sum $ map numOfCombMils [1..100]
