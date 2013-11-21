e4322f5ec67bd1bce2a7f6dce98db90160112de4
swrangsar basumatary <swrangsar@example.com>
2013-11-22 00:50:44 +0530

    commit at 20131122005044

--- squareRootConv.hs
+++ squareRootConv.hs
@@ -1,28 +1,30 @@
 import RhoFactors
-import Data.List.Ordered
+import Data.List
 
 
 
-rhoFactors :: (Integral a) => a -> [a]
-rhoFactors = RhoFactors.rhoFactors
+rhoFacts :: (Integral a) => a -> [a]
+rhoFacts = RhoFactors.rhoFactors
 
-squareRootConv :: (Integral a) => a -> (a,a)
-squareRootConv n = frec (2,1) n
-    where frec x r
-            | r > 1     = frec y (r-1)
-            | otherwise = z
+innerConv :: (Integral a) => a -> [(a,a)]
+innerConv n = irec (2,1) 1
+    where irec x r
+            | r <= n    = x:irec y (r+1)
+            | otherwise = []
             where (a,b)     = x
                   y         = addFrac (2,1) (b,a)
-                  z         = addFrac (1,1) (b,a)
+
+squareRootConv :: (Integral a) => (a,a) -> (a,a)
+squareRootConv (a,b) = addFrac (1,1) (b,a)
 
 
 addFrac :: (Integral a) => (a,a) -> (a,a) -> (a,a)
 addFrac (a,b) (c,d) = (m,n)
-    where xs    = rhoFactors $ a*d + b*c
-          ys    = rhoFactors $ b*d
-          cs    = isect xs ys
-          m     = product $ 1:(minus xs cs)
-          n     = product $ 1:(minus ys cs)
+    where xs    = rhoFacts $ a*d + b*c
+          ys    = rhoFacts $ b*d
+          cs    = intersect xs ys
+          m     = product $ 1:(xs \\ cs)
+          n     = product $ 1:(ys \\ cs)
 
 num2DigitsR :: (Integral a) => a -> [a]
 num2DigitsR x
@@ -32,12 +34,12 @@ num2DigitsR x
           rem  = x `mod` 10
 
 
-isBiggerNum :: (Integral a) => a -> Bool
-isBiggerNum n = l > k
-    where (a,b)     = squareRootConv n
-          l         = length $ num2DigitsR a
+isBiggerNum :: (Integral a) => (a,a) -> Bool
+isBiggerNum (a,b) = l > k
+    where l         = length $ num2DigitsR a
           k         = length $ num2DigitsR b
     
-    
 
-reqdCount = length $ filter isBiggerNum [1..1000]
+
+
+reqdCount = length $ filter isBiggerNum $ map squareRootConv $ innerConv 1000
