8239e1b26a16bd6633eeb86618269048f1533f65
swrangsar basumatary <swrangsar@example.com>
2013-12-11 15:12:42 +0530

    commit at 20131211151242

--- primePairSet.hs
+++ primePairSet.hs
@@ -17,19 +17,33 @@ isPrimePair a b = (isPrime x) && (isPrime y)
 primes :: (Integral a) => [a]
 primes = filter isPrime (2:[3,5..])
 
-primes' = takeWhile (< 10000) primes
+primes' = takeWhile (< 25000) primes
 
 pset p = filter (isPrimePair p) primes'
 
 
 
 set = [[a,b,c,d,e]| a <- primes', 
-    b <- (takeWhile (< a) primes'),
-    isPrimePair a b,
+    b <- (pset a),
     let depth1 = intersect (pset a) (pset b),
     c <- depth1,
-    d <- (takeWhile (< c) depth1),
-    isPrimePair c d,
+    d <- filter (isPrimePair c) depth1,
     let depth2 = intersect (filter (isPrimePair c) depth1) (filter (isPrimePair d) depth1),
     e <- depth2]
+
+setnew = [(sum [a,b,c,d,e]) | a <- take 11 primes',
+    b <- dropWhile (<= a) primes',
+    isPrimePair a b,
+    c <- dropWhile (<= b) primes',
+    isPrimePair a c,
+    isPrimePair b c,
+    d <- dropWhile (<= c) primes',
+    isPrimePair a d,
+    isPrimePair b d,
+    isPrimePair c d,
+    e <- dropWhile (<= d) primes',
+    isPrimePair a e,
+    isPrimePair b e,
+    isPrimePair c e,
+    isPrimePair d e]
 
