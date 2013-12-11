802502670a1fea71b4d3d7e9cef04557daa55abe
swrangsar basumatary <swrangsar@example.com>
2013-12-11 17:01:37 +0530

    commit at 20131211170137

--- primePairSet.hs
+++ primePairSet.hs
@@ -1,47 +1,47 @@
 import RabinMiller
 import Data.List
 
+
 isPrime :: (Integral a) => a -> Bool
 isPrime = RabinMiller.isRabinMillerPseudoPrime
 
 
-isPrimePair :: (Integral a) => a -> a -> Bool
+
+
+isPrimePair :: Int -> Int -> Bool
 isPrimePair a b = (isPrime x) && (isPrime y)
-    where   x = a*p + b
-            y = b*q + a
-            p = head $ dropWhile (<b) $ map (10^) [0..]
-            q = head $ dropWhile (<a) $ map (10^) [0..]
+    where   x = readInt $ show a ++ show b
+            y = readInt $ show b ++ show a
+            readInt = read:: String -> Int
+
         
         
 
 primes :: (Integral a) => [a]
 primes = filter isPrime (2:[3,5..])
 
+
 primes' = takeWhile (< 25000) primes
 
-pset p = filter (isPrimePair p) primes'
 
 
 
-set = [[a,b,c,d,e]| a <- primes', 
-    b <- (pset a),
-    let depth1 = intersect (pset a) (pset b),
-    c <- depth1,
-    d <- filter (isPrimePair c) depth1,
-    let depth2 = intersect (filter (isPrimePair c) depth1) (filter (isPrimePair d) depth1),
-    e <- depth2]
 
-setnew = [(sum [a,b,c,d,e]) | a <- take 11 primes',
-    b <- dropWhile (<= a) primes',
+set = [(sum [a,b,c,d,e]) | a <- take 11 primes',
+    let depth1 = dropWhile (<= a) primes',
+    b <- depth1,
     isPrimePair a b,
-    c <- dropWhile (<= b) primes',
+    let depth2 = dropWhile (<= b) primes',
+    c <- depth2,
     isPrimePair a c,
     isPrimePair b c,
-    d <- dropWhile (<= c) primes',
+    let depth3 = dropWhile (<= c) primes',
+    d <- depth3,
     isPrimePair a d,
     isPrimePair b d,
     isPrimePair c d,
-    e <- dropWhile (<= d) primes',
+    let depth4 = dropWhile (<= d) primes',
+    e <- depth4,
     isPrimePair a e,
     isPrimePair b e,
     isPrimePair c e,
