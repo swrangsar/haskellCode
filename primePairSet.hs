87b3f2d37624c67b4bdc9534f1a9fc0a755110e2
swrangsar basumatary <swrangsar@example.com>
2013-12-13 12:28:31 +0530

    commit at 20131213122831

--- primePairSet.hs
+++ primePairSet.hs
@@ -14,8 +14,7 @@ isPrimePair a b = (isPrime x) && (isPrime y)
             y = readInt $ show b ++ show a
             readInt = read:: String -> Int
 
-        
-        
+      
 
 primes :: (Integral a) => [a]
 primes = filter isPrime (2:[3,5..])
@@ -27,23 +26,14 @@ primes' = takeWhile (< 25000) primes
 
 
 
-set = [(sum [a,b,c,d,e]) | a <- take 11 primes',
-    let depth1 = dropWhile (<= a) primes',
+
+set = [[a,b,c,d,e] | a <- primes',
+    let depth1 = filter (isPrimePair a) $ dropWhile (<= a) primes',
     b <- depth1,
-    isPrimePair a b,
-    let depth2 = dropWhile (<= b) primes',
+    let depth2 = filter (isPrimePair b) $ dropWhile (<= b) depth1,
     c <- depth2,
-    isPrimePair a c,
-    isPrimePair b c,
-    let depth3 = dropWhile (<= c) primes',
+    let depth3 = filter (isPrimePair c) $ dropWhile (<= c) depth2,
     d <- depth3,
-    isPrimePair a d,
-    isPrimePair b d,
-    isPrimePair c d,
-    let depth4 = dropWhile (<= d) primes',
-    e <- depth4,
-    isPrimePair a e,
-    isPrimePair b e,
-    isPrimePair c e,
-    isPrimePair d e]
+    let depth4 = filter (isPrimePair d) $ dropWhile (<= d) depth3,
+    e <- depth4]
 
