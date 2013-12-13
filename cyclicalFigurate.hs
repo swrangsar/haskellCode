a5de9db449de90250acdfb371d5e7143c0fce1d9
swrangsar <swrangsar@aol.com>
2013-12-13 15:29:17 +0530

    at pclab prakash fellowship calling...

--- cyclicalFigurate.hs
+++ cyclicalFigurate.hs
@@ -1,5 +1,4 @@
 
-
 triangles :: (Integral a) => [a]
 triangles = map triangle [1..]
     where triangle n = (n * (n+1)) `div` 2
@@ -10,5 +9,22 @@ squares :: (Integral a) => [a]
 squares = map square [1..]
     where square n = n * n
 
+pentagonals :: (Integral a) => [a]
+pentagonals = map pentagonal [1..]
+    where pentagonal n = (n * (3*n-1)) `div` 2
+
+
+hexagonals :: (Integral a) => [a]
+hexagonals = map hexagonal [1..]
+    where hexagonal n = n * (2*n-1)
+    
+heptagonals :: (Integral a) => [a]
+heptagonals = map heptagonal [1..]
+    where heptagonal n = (n * (5*n-3)) `div` 2
+    
+
+octagonals :: (Integral a) => [a]
+octagonals = map octagonal [1..]
+    where octagonal n = n * (3*n-2)
 
 
