0c752183420800533ff72c5545c99f60ca0f71e4
swrangsar basumatary <swrangsar@example.com>
2013-12-16 17:02:38 +0530

    commit at 20131216170238

--- cyclicalFigurate.hs
+++ cyclicalFigurate.hs
@@ -1,60 +1,62 @@
+import Data.List
 
 
-triangles :: (Integral a) => [a]
-triangles = map (1,) $ fourDigits $ map triangle [1..]
-    where triangle n = (n * (n+1)) `div` 2
-
-
-
-squares :: (Integral a) => [a]
-squares = map (2,) $ fourDigits $ map square [1..]
-    where square n = n * n
-
-pentagonals :: (Integral a) => [a]
-pentagonals = map (3,) $ fourDigits $ map pentagonal [1..]
-    where pentagonal n = (n * (3*n-1)) `div` 2
-
-
-hexagonals :: (Integral a) => [a]
-hexagonals = map (4,) $ fourDigits $ map hexagonal [1..]
-    where hexagonal n = n * (2*n-1)
-    
-heptagonals :: (Integral a) => [a]
-heptagonals = map (5,) $ fourDigits $ map heptagonal [1..]
-    where heptagonal n = (n * (5*n-3)) `div` 2
-    
-
-octagonals :: (Integral a) => [a]
-octagonals = map (6,) $ fourDigits $ map octagonal [1..]
-    where octagonal n = n * (3*n-2)
 
 
 fourDigits :: (Integral a) => [a] -> [a]
 fourDigits l = takeWhile (< 10000) $ dropWhile (<1000) l
 
 
-isPair :: (Integral a) => a -> a -> Bool
-isPair a b = (inSet a b) || (inSet b a)
+
+triangles :: (Integral a) => [(a,a)]
+triangles = map (\x -> (1,x)) $ fourDigits $ map triangle [1..]
+    where triangle n = (n * (n+1)) `div` 2
 
 
-inSet :: (Integral a) => a -> a -> Bool
+squares :: (Integral a) => [(a,a)]
+squares = map (\x -> (2,x)) $ fourDigits $ map square [1..]
+    where square n = n * n
+
+pentagonals :: (Integral a) => [(a,a)]
+pentagonals = map (\x -> (3,x)) $ fourDigits $ map pentagonal [1..]
+    where pentagonal n = (n * (3*n-1)) `div` 2
+
+
+hexagonals :: (Integral a) => [(a,a)]
+hexagonals = map (\x -> (4,x)) $ fourDigits $ map hexagonal [1..]
+    where hexagonal n = n * (2*n-1)
+    
+heptagonals :: (Integral a) => [(a,a)]
+heptagonals = map (\x -> (5,x)) $ fourDigits $ map heptagonal [1..]
+    where heptagonal n = (n * (5*n-3)) `div` 2
+    
+
+octagonals :: (Integral a) => [(a,a)]
+octagonals = map (\x -> (6,x)) $ fourDigits $ map octagonal [1..]
+    where octagonal n = n * (3*n-2)
+
+
+
+
+inSet :: (Integral a) => (a,a) -> (a,a) -> Bool
 inSet m n
     | x == y    = True
     | otherwise = False
-    where x = mod m 100
-          y = div n 100
+    where x = mod (snd m) 100
+          y = div (snd n) 100
 
 
-candidates = [[a,b,c,d,e,f] | 
-    a <- fourDigits triangles,
-    b <- (filter (isPair a) $ fourDigits squares),
-    c <- (filter (isPair b) $ fourDigits pentagonals),
-    d <- (filter (isPair c) $ fourDigits hexagonals),
-    e <- (filter (isPair d) $ fourDigits heptagonals),
-    f <- (filter (isPair e) $ fourDigits octagonals)]
+notTriangles = squares ++ pentagonals ++ hexagonals ++ heptagonals ++ octagonals
+
+candidates = [l | 
+    a <- triangles,
+    b <- (filter (inSet a) notTriangles),
+    c <- (filter (inSet b) notTriangles),
+    d <- (filter (inSet c) notTriangles),
+    e <- (filter (inSet d) notTriangles),
+    f <- (filter (inSet e) notTriangles),
+    let l = [a,b,c,d,e,f],
+    (length (nub (map fst l))) > 5,
+    inSet f a]
     
-    
-isCyclic :: (Integral a) => [a] -> Bool
-isCyclic l = 
-    where   rec (x:xs) 
-    
+ answer = sum $ map snd $ head candidates
