a498f78f8d1ff908f88c891ea58771a0de0061cd
swrangsar basumatary <swrangsar@example.com>
2013-12-17 13:38:05 +0530

    commit at 20131217133805

--- cubicPermutations.hs
+++ cubicPermutations.hs
@@ -10,17 +10,25 @@ num2Digits x
     where quot = x `div` 10
           rem  = x `mod` 10
 
+diglen :: (Integral a) => a -> Int
+diglen x = length $ num2Digits x
+
+cubes :: (Integral a) => [a]
+cubes = map (^3) [1..]
 
 
-candidates = [l | a <- [1..100000],
-    let len = length $ num2Digits a,
-    b <- [(a+1)..(10^len-1)],
-    c <- [(b+1)..(10^len-1)],
-    d <- [(c+1)..(10^len-1)],
-    e <- [(d+1)..(10^len-1)],
+candidates = [l | a <- cubes,
+    let cubes1 = dropWhile (<= a) cubes,
+    b <- (takeWhile (\x  -> (diglen x) == (diglen a)) cubes1),
+    let cubes2 = dropWhile (<= b) cubes1,
+    c <- (takeWhile (\x  -> (diglen x) == (diglen a)) cubes2),
+    let cubes3 = dropWhile (<= c) cubes2,
+    d <- (takeWhile (\x  -> (diglen x) == (diglen a)) cubes3),
+    let cubes4 = dropWhile (<= d) cubes3,
+    e <- (takeWhile (\x  -> (diglen x) == (diglen a)) cubes4),
     let l = [a,b,c,d,e],
-    let cs = map (num2Digits.(^3)) l,
-    length (nub (map length cs)) == 1,
+    let cs = map num2Digits l,
+    (length $ nub $ map sum cs) == 1,
     (length $ nub $ map length $ map (filter (==0)) cs) == 1,
-    length (nub (map sum cs)) == 1,
-    (length $ nub $ map product $ map (filter (/=0)) cs) == 1]
+    (length $ nub $ map product $ map (filter (/=0)) cs) == 1
+    ]
